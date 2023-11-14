27d8c9317a6f1c9a877554639912a073419c505a
Jovica DUFOUR <jdufour@made-f0Ar11s5.clusters.42paris.fr>
2023-11-14 14:07:35 +0100

    first commit

---
+++ minishell.h
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jdufour <jdufour@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/14 13:46:13 by jdufour           #+#    #+#             */
+/*   Updated: 2023/11/14 13:56:34 by jdufour          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+
+// System Libraries
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <limits.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <fcntl.h>
+# include <sys/syscall.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <sys/stat.h>
+# include <dirent.h>
+# include <errno.h>
+# include <sys/ioctl.h>
+# include <termios.h>
+# include <curses.h>
+# include <term.h>
+
+// // Personnal Libraries
+
+// # include "libft/libft.h"
+
+
+#endif
