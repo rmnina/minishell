7df50ac2c0be9a335f70a14fc28115e8162a0dee
jovica pc fixe <jovicadufour@gmail.com>
2023-11-17 02:31:28 +0100

    avoiding a segfault case in quotes.c

--- lexer/quotes.c
+++ lexer/quotes.c
@@ -6,7 +6,7 @@
 /*   By: jdufour <jdufour@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 15:01:50 by jdufour           #+#    #+#             */
-/*   Updated: 2023/11/17 02:15:14 by jdufour          ###   ########.fr       */
+/*   Updated: 2023/11/17 02:26:15 by jdufour          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,22 +48,24 @@ void	embedded_quotes(char *line, t_quotes *quotes)
 	int	i;
 
 	i = 0;
+	quotes->single_embedded = 0;
+	quotes->double_embedded = 0;
 	while (line[i])
 	{
 		if (line[i] == SINGLE_QUOTE)
 		{
 			i++;
-			while (line[i] != SINGLE_QUOTE)
+			while (line[i] && line[i] != SINGLE_QUOTE)
 			{
 				if (line[i] == DOUBLE_QUOTE)
 					quotes->double_embedded++;
 				i++;
 			}
 		}
-		else if (line[i] == DOUBLE_QUOTE)
+		if (line[i] == DOUBLE_QUOTE)
 		{
 			i++;
-			while (line[i] != DOUBLE_QUOTE)
+			while (line[i] && line[i] != DOUBLE_QUOTE)
 			{
 				if (line[i] == SINGLE_QUOTE)
 					quotes->single_embedded++;
@@ -74,20 +76,37 @@ void	embedded_quotes(char *line, t_quotes *quotes)
 	}
 }
 
-// int	main(int argc, char **argv)
+// int	error_quotes(t_quotes *quotes)
 // {
-// 	(void)argc;
-// 	t_quotes quotes;
-	
-// 	quotes_count(argv[1], &quotes);
-// 	embedded_quotes(argv[1], &quotes);
-// 	// printf("single embedded = %d\n", quotes.single_embedded);
-// 	// printf("double embedded = %d\n", quotes.double_embedded);
-// 	// printf("single = %d\n", quotes.single_quotes);
-// 	// printf("double = %d\n", quotes.double_quotes);
-// 	if (error_quotes(&quotes))
-// 		printf("error parsing\n");
-// 	else
-// 		printf("%s\n", argv[1]);
+// 	if (quotes->single_quotes % 2 == 0)
+// 	{
+// 		if ((quotes->double_quotes % 2 != 0 && quotes->double_embedded % 2 == 0) 
+// 		|| (quotes->double_quotes % 2 == 0 && quotes->double_embedded % 2 != 0))
+// 			return (1);
+// 	}
+// 	else if (quotes->double_quotes % 2 == 0)
+// 	{
+// 		if ((quotes->single_quotes % 2 != 0 && quotes->single_embedded % 2 == 0) 
+// 		|| (quotes->single_quotes % 2 == 0 && quotes->single_embedded % 2 != 0))
+// 			return (1);
+// 	}
 // 	return (0);
 // }
+
+int	main(int argc, char **argv)
+{
+	(void)argc;
+	t_quotes quotes;
+	
+	quotes_count(argv[1], &quotes);
+	embedded_quotes(argv[1], &quotes);
+	printf("single embedded = %d\n", quotes.single_embedded);
+	printf("double embedded = %d\n", quotes.double_embedded);
+	printf("single = %d\n", quotes.single_quotes);
+	printf("double = %d\n", quotes.double_quotes);
+	if (error_quotes(&quotes))
+		printf("error parsing\n");
+	else
+		printf("%s\n", argv[1]);
+	return (0);
+}
