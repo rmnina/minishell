a16654e1753d4b6348c7d489cddd993fd4328a2c
jdufour <jdufour@made-f0Dr7s3.clusters.42paris.fr>
2024-01-08 14:46:45 +0100

    update tester

--- tester/minishell_tester.py
+++ tester/minishell_tester.py
@@ -11,26 +11,20 @@ def run_test(command):
 def main(input_file, output_diff_file):
     with open(input_file, 'r') as file:
         commands = file.readlines()
-
     with open(output_diff_file, 'w') as diff_file:
         diff_file.write("")
-
     for idx, command in enumerate(commands, start=1):
         command = command.strip()
-
         shell_output = run_test(command)
-
         minishell_output = run_test(f'./minishell {command}')
 
         if shell_output != minishell_output:
             diff = difflib.unified_diff(shell_output.splitlines(), minishell_output.splitlines())
             diff_text = '\n'.join(diff)
-
             with open(output_diff_file, 'a') as diff_file:
                 diff_file.write(f"Test {idx} - Command: {command}\n")
                 diff_file.write(diff_text)
                 diff_file.write("\n\n")
-
             print(f"KO - Test {idx} - Diff saved to {output_diff_file}")
         else:
             print(f"OK - Test {idx}")
